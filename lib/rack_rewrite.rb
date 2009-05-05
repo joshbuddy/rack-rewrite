362594d4b7ed936461e1a897e458db5ce97fe945
Joshua Hull <joshbuddy@gmail.com>
2009-05-04 22:00:48 -0700

    broke condition_set into its own file

--- lib/rack_rewrite.rb
+++ lib/rack_rewrite.rb
@@ -1,6 +1,7 @@
 $:.unshift(File.dirname(__FILE__))
 require 'rack'
 require 'rack_rewrite/actions'
+require 'rack_rewrite/condition_set'
 
 module Rack
   class Rewrite
@@ -8,43 +9,6 @@ module Rack
     FailError = Class.new(RuntimeError)
     
     attr_accessor :path, :redirect, :headers
-    
-    class ConditionSet
-      
-      attr_reader :parent_set, :actions, :conditions
-      
-      def initialize(parent_set, conditions = nil)
-        @parent_set = parent_set
-        @conditions = conditions
-        @actions = []
-      end
-
-      def satisfied?(env)
-        if conditions
-          
-          uri_ok = conditions.key?(:uri) ? conditions[:uri] === env['PATH_INFO'] : true
-          method_ok = conditions.key?(:method) ? conditions[:method] === env['REQUEST_METHOD'].downcase : true
-          host_ok = conditions.key?(:host) ? conditions[:host] === env['HTTP_HOST'] : true
-          port_ok = conditions.key?(:port) ? conditions[:port] === env['SERVER_PORT'].to_i : true
-          scheme_ok = conditions.key?(:scheme) ? conditions[:scheme] === env['rack.url_scheme'] : true
-          if conditions.key?(:params)
-            req = Rack::Request.new(env)
-            params_ok = true
-            conditions[:params].each do |key, test|
-              params_ok = test === req.params[key.to_s]
-              break unless params_ok
-            end
-          else
-            params_ok = true
-          end
-
-          uri_ok && method_ok && host_ok && port_ok && scheme_ok && params_ok
-        else
-          true
-        end
-      end
-      
-    end
     
     def initialize(app, options = {}, &block)
       @app = app
